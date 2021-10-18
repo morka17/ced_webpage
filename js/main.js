fc5c28b75ff3c607dc1d0ab94be1af56e8ae5fe5
morka17 <joshuamorka19@gmail.com>
2021-10-18 00:59:36 -0700

    test commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var topContainer = document.getElementById('top-container');
+
+topContainer.innerHTML = `
+<div class="row">
+            <div class="col-2">
+                <h1>
+                    Osagie<br /> For President
+                </h1>
+            </div>
+            <div class="col-9">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Manifesto</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Biography</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-1">
+                <a class="btn btn-primary" href="">Sponsorship</a>
+            </div>
+        </div>
+`;
